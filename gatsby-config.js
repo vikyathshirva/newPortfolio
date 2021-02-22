104fe9a6ab209b1683a9d78535f50beb95b7b89f
Vikyath Shirva <vicky.fames@gmail.com>
2021-02-22 23:46:15 +0530

    New files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    setTitle: `Vikyath Shirva`,
     
     siteTitleAlt: `Vikyath Shriva | Software Dev`,
   },
